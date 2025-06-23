341a4654f7e4adc3b8b1248cfadeb7c69fa0eb5b
kaka-zuumi <125692681+kaka-zuumi@users.noreply.github.com>
2025-06-23 12:42:36 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
+#with open("README.md", 'r') as f:
+#    long_description = f.read()
 
 setup(
    name='gmthrash',
    version='1.0',
    description='Forward Convolution Calculations for Crossed Molecular Beams Experiments',
    license="MIT",
-   long_description=long_description,
+#   long_description=long_description,
    author='Kazuumi Fujioka',
    author_email='kazuumi@hawaii.edu',
    url="https://github.com/kaka-zuumi/GMTHRASH",
