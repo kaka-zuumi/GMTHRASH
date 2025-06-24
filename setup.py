a827c956de502b79eb73e1ab942c185e698b1229
kaka-zuumi <125692681+kaka-zuumi@users.noreply.github.com>
2025-06-23 14:08:58 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
    author='Kazuumi Fujioka',
    author_email='kazuumi@hawaii.edu',
    url="https://github.com/kaka-zuumi/GMTHRASH",
-   packages=['gmthrash'],  #same as name
+#   packages=['gmthrash'],  #same as name
    install_requires=['pandas', 'numpy', 'scipy', 'matplotlib', 'sys', 're', 'customtkinter', 'tkinter'], #external packages as dependencies
    scripts=[
             'GMTHRASH.py',
