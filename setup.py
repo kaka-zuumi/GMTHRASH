bf894c7ed9beb01f5f8dba0e242f05a02e44174c
kaka-zuumi <125692681+kaka-zuumi@users.noreply.github.com>
2025-07-10 18:24:12 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='gmthrash',
-   version='1.0.4',
+   version='1.0.5',
    description='Forward Convolution Calculations for Crossed Molecular Beams Experiments',
    license="MIT",
 #   long_description=long_description,
